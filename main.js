64b8982910b31c401568fca15bdcfbbcc3935307
AtticusFetch <lordofthecucumbers@gmail.com>
2015-03-19 13:09:23 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+var currentTitle = '';
+
+$(document).ready(function(){
+    init();
+    clickMenuItem();
+});
+
+function init(){
+    $('body').css('opacity','1');
+    setTitle('main');
+    $('.header').css('height',$('.wrapper .header #' + currentTitle).outerHeight(true) + 'px');
+            /*.css({left: $('.wrapper .menu ul').outerWidth(true)})
+                .css({right: 'auto'})*/
+    //alert($('.wrapper .menu ul li').outerWidth(true));
+}
+
+function setTitle(titleID){
+    currentTitle = titleID;
+    $('.wrapper .header #' + titleID).addClass('visibleTitle')
+        .css({left: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2})
+            .css({right: ($('.header').outerWidth(true) - $(this).outerWidth(true))/2});
+}
+
+function clickMenuItem(){
+    $('.wrapper .header').click(function(){
+        removeTitle(currentTitle);
+        $('#' + $(this).attr('class') + 'Content').removeClass($(this).attr('class') + 'Visible');
+        init();
+    });
+    $('.wrapper .menu ul li').click(function(){
+        //alert($(this).attr('class'));
+        $('#' + $(this).attr('class') + 'Content').addClass($(this).attr('class') + 'Visible');
+        $('#' + $(this).attr('class') + 'Content').css('left', ($(document).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 +'px')
+            .css('right',($(document).width() - $('#' + $(this).attr('class') + 'Content').outerWidth())/2 + 'px');
+        removeTitle(currentTitle);
+        setTitle($(this).attr('class'));
+        $(this).addClass('rotate')
+            .mouseout(function(){
+                $(this).removeClass('rotate');
+        });
+    });
+}
+
+function removeTitle(titleID){
+    $('.wrapper .header #' + titleID).removeClass('visibleTitle');
+}
+
+/**
+ * Created by Ivan_Iankovskyi on 3/18/2015.
+ */
